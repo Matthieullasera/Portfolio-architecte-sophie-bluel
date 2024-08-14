316f2a083326d81f5e00c31962832f6a59fcae57
Matthieu_llasera <matthieullasera@gmail.com>
2024-08-14 12:21:59 +0200

    - add anchor for contact, projets, h1 - creation of function who show the gallery with api and not html - rename filtre_dynamique in index because because this file is useful not only for dynamic filter - remove of index amdin and modif on index to prepare the page for the change in admin page

--- FrontEnd/assets/javascripts/connexion.js
+++ FrontEnd/assets/javascripts/connexion.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', main);
 
 function main() {
     checkForm();
-    removeTokenForLogout();
 }
 
 function checkForm() {
@@ -16,49 +15,38 @@ function checkForm() {
         const formatMail = /^[a-z0-9-_.]+@[a-z0-9]+\.[a-z]{2,}$/;
 
         console.log(mail, password); 
-
-        if (formatMail.test(mail) && password !== '') {
-            try {
-                const response = await fetch('http://localhost:5678/api/users/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        email: mail,
-                        password: password
-                    })
-                });
-
-                const result = await response.json();
-                console.log(result);
-                
-                if (response.ok) {
-                    console.log("Login successful");
-                    localStorage.setItem('authToken', result.token);
-                    console.log("Token stored in local storage");
-                    window.location.href = 'index_admin.html';
-
-                    
-                } else {
-                    console.log("Login failed", result);
-                }
-            } catch (error) {
-                console.error("Error during fetch", error);
-            }
-        } else {
+        if (!formatMail.test(mail) || password === '') {
             console.log("Invalid email or empty password");
+            return;
+        }
+        
+        try {
+            const response = await fetch('http://localhost:5678/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: mail,
+                    password: password
+                })
+            });
+        
+            const result = await response.json();
+            console.log(result);
+        
+            if (response.ok) {
+                console.log("Login successful");
+                localStorage.setItem('authToken', result.token);
+                console.log("Token stored in local storage");
+                window.location.href = 'index.html';
+            } else {
+                console.log("Login failed", result);
+            }
+        } catch (error) {
+            console.error("Error during fetch", error);
         }
     });
 }
 
-function removeTokenForLogout() {
-    const logoutButton = document.getElementById('logout');
-    if (logoutButton) {
-        logoutButton.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            localStorage.removeItem('authToken');
-            window.location.href = 'index.html'; 
-        });
-    }
-}
+
---
+++ FrontEnd/assets/javascripts/index.js
@@ -0,0 +1,104 @@
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des informations des images');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+function displayGallery(pictures) {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = ''; 
+
+    pictures.forEach(picture => {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        img.src = picture.imageUrl; 
+        img.alt = picture.description || ''; 
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = picture.title || ''; 
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
+}
+
+function createFilterButtons(categories) {
+    const filtersContainer = document.querySelector('.filters'); 
+    const buttons = [];
+    categories.forEach(filter => {
+        const button = document.createElement('button'); 
+        button.id = filter.id; 
+        button.className = 'filter-button'; 
+        const span = document.createElement('span'); 
+        span.textContent = filter.name; 
+        button.appendChild(span); 
+        filtersContainer.appendChild(button);
+        buttons.push(button); 
+    });
+
+    return buttons; 
+}
+
+function createGalleryMyProjects(works) {
+    const galleryContainer = document.querySelector('.gallery');
+    const images = [];
+    works.forEach(filter => {
+        const figure = document.createElement('figure');
+        const image = document.createElement('img');
+        image.src = filter.imageUrl;
+        image.alt = filter.title;
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = filter.title;
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        galleryContainer.appendChild(figure);
+        return figure;
+    })
+    return images;
+}
+async function filterEvent(buttons, list_images) {
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const categoryId = button.id;
+            console.log('Category ID:', categoryId);
+
+            let picturesToDisplay;
+            if (categoryId === 'all') {
+                picturesToDisplay = list_images;
+            } else {
+                picturesToDisplay = list_images.filter(image => image.categoryId.toString() === categoryId);
+            }
+            console.log('Pictures to Display:', picturesToDisplay);
+
+            displayGallery(picturesToDisplay);
+        });
+    });
+}
+
+function removeTokenForLogout() {
+    const logoutButton = document.getElementById('logout');
+    if (logoutButton) {
+        logoutButton.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            localStorage.removeItem('authToken');
+            window.location.href = 'index.html'; 
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const categories = await fetchData('http://localhost:5678/api/categories');
+    let list_images = await fetchData('http://localhost:5678/api/works');
+    categories.unshift({ id: 'all', name: 'Tous' });
+    console.log(list_images);
+    const images = createGalleryMyProjects(list_images);
+    const buttons = createFilterButtons(categories);
+    filterEvent(buttons, list_images);
+});
